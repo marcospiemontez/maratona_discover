d1d4133f8f9529ac5202e0dbc288e140db1b31fd
Marcos Piemontez <marcos.piemontez1@gmail.com>
2021-12-20 20:01:26 -0400

    ✨ NEW FEATURE: implementando a integração com back-end

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ server.set('view engine', 'ejs')
 
 server.use(express.static("public"))
 
+// Liberação do req.body
+server.use(express.urlencoded({ extended: true }))
+
 server.use(routes)
 
 server.listen(3001, () => console.log('Server on'))
